0c7e5327b0e3947c3b4f1c49d54569e85f0365ff
Barleysack <hookypooky@naver.com>
2021-08-11 17:17:45 +0900

    Little copy from assignments

--- DenseNetMNIST.py
+++ DenseNetMNIST.py
@@ -14,7 +14,8 @@ from torch.utils import data as D
 from torch.utils.data.sampler import SubsetRandomSampler
 import random
 import torchsummary
-
+from torchvision import datasets
+import matplotlib.pyplot as plt
 print(torch.__version__)
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") 
 print(device)
@@ -27,7 +28,13 @@ num_epoch = 300
 
 #데이터 어그멘테이션은 스킵합니다. MNIST는 그 자체로 충분할 터.
 
-#내일은 데이터 로딩 부분을 구현하겠습니다. 
+from torchvision import datasets
+mnist_train = datasets.MNIST(root='./data/',train=True,transform=transforms.ToTensor(),download=True)
+mnist_test = datasets.MNIST(root='./data/',train=False,transform=transforms.ToTensor(),download=True)
+print ("mnist_train:\n",mnist_train,"\n")
+print ("mnist_test:\n",mnist_test,"\n")
+print ("Done.")
+#마침 적당한 셀이 과제에 있어 뜯어왔다. 
 
 
 class ConvReluBatch(nn.Module):                     #DenseNet에 이런 뭉태기가 많이 쓰인다고, 동시에 구현하면 좋다길래 해뒀습니다. 
@@ -62,4 +69,24 @@ class bottleneck_layer(nn.Sequential): #일어나서 여기 리뷰 매우 필요
       return bottleneck_output
 
 
+
+
+
+#평가까지 해주는 편안한 셀이 있는데 안쓸 이유가 없죠
+n_sample = 25
+sample_indices = np.random.choice(len(mnist_test.targets),n_sample,replace=False)
+test_x = mnist_test.data[sample_indices]
+test_y = mnist_test.targets[sample_indices]
+with torch.no_grad():
+    C.eval() # to evaluation mode #put model name in
+    y_pred = C.forward(test_x.view(-1,1,28,28).type(torch.float).to(device)/255.)
+y_pred = y_pred.argmax(axis=1)
+plt.figure(figsize=(10,10))
+for idx in range(n_sample):
+    plt.subplot(5, 5, idx+1)
+    plt.imshow(test_x[idx], cmap='gray')
+    plt.axis('off')
+    plt.title("Pred:%d, Label:%d"%(y_pred[idx],test_y[idx]))
+plt.show()    
+print ("Done")
     
